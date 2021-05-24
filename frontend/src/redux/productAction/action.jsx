03bad0fecfe43126978d9e2bdce8e09bfc3790f4
AbirBenSaiid <84208221+AbirBenSaiid@users.noreply.github.com>
2021-05-25 00:59:43 +0100

    product create front/ get curret user pets

--- frontend/src/redux/productAction/action.jsx
+++ frontend/src/redux/productAction/action.jsx
@@ -3,17 +3,18 @@ import axios from "axios";
 
 //ADD Pet
 export const addProduct = (product, history, shopID) => async (dispatch) => {
-  dispatch({ type: LOAD_PRODUCT });
-  try {
+    dispatch({ type: LOAD_PRODUCT });
+    try {
     let result = await axios.post(`${process.env.PUBLIC_URL}/api/product/create`, {
-      product,
-      shopID, 
-    });
+        product,
+        shopID, 
+    } );
     dispatch({ type: ADD_PRODUCT, payload: result.data });
     
-  } catch (error) {
-    dispatch({ type: FAIL_PRODUCT, payload: error.response.data });
-  }
+    } catch (error) {
+        console.log(error.response)
+    dispatch({ type: FAIL_PRODUCT, payload: error.response.data.errors });
+    }
 };
 
 export const videErrors = () => {
